d3d928595b97ef60e187a49b2de9dbfb53baee38
Stephen Bastians <stevejb71@gmail.com>
2017-09-19 19:36:50 +0100

    Add a view model

---
+++ Pricer/ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Pricer
+{
+    using Model;
+
+    public class ViewModel
+    {
+        public string ApiKey { get; set; }
+        public List<Stock> Stocks { get; set; }
+    }
+}
